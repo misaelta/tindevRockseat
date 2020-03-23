d089d3d3952264e69a7197fb862e6b49b8bfb441
misaelta <misaelt.a@hotmail.com>
2020-03-23 19:11:14 -0300

    atualizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 const routes = require('./routes')
 const cors = require('cors')
 const server = express();
-mongoose.connect('mongodb+srv://misael:misael@cluster0-vx1pb.mongodb.net/omnistack8?retryWrites=true&w=majority',{useNewUrlParser:true})
+mongoose.connect('mongodb+srv://USER:PASSWORD@cluster0-vx1pb.mongodb.net/NOMEDOBANCO?retryWrites=true&w=majority',{useNewUrlParser:true})
 
 server.use(cors());
 server.use(express.json());
